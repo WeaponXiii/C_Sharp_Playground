ea99e73ccc27a372aa82f232dab0dc1bd083feae
WeaponXiii <samaun.xiii@gmail.com>
2020-04-06 11:17:12 +0200

    Add: Generic extension method demo.

---
+++ C_Sharp_Playground/GenericExtension/Classes/ExtMetods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C_Sharp_Playground.GenericExtension.classes
+{
+    
+    public static class ExtMetods
+    {
+        public static Action<object> Print = (o) => Console.WriteLine(o);
+        public static void MakeSoundExt<T>(this T animal, Action<int> act) where T: IAnimal {
+            switch (animal)
+            { 
+                case Horse h:
+                    Console.WriteLine("Got a horse"); break;
+                case Dog d:
+                    Console.WriteLine("Got a dog"); break;
+                default:
+                    break;
+                    
+            }
+            animal.makeSound();
+            act?.Invoke(10);
+        }
+    }
+}
---
+++ C_Sharp_Playground/GenericExtension/Classes/IAnimal.cs
@@ -0,0 +1,7 @@
+﻿namespace C_Sharp_Playground.GenericExtension.classes
+{
+    public interface IAnimal
+    {
+        void makeSound();
+    }
+}
