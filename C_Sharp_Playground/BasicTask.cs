02cfe567e8756eb1e2f3c4b97b5ca41e8730fd02
WeaponXiii <samaun.xiii@gmail.com>
2024-11-26 22:19:42 +0100

    Add: Basic task example with cancelation token.

---
+++ C_Sharp_Playground/BasicTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Playground.TAP
+{
+    class BasicTask
+    {
+        public async Task DoTask()
+        {
+            var cts = new CancellationTokenSource();
+
+            // Create a task
+            Task<int> task = Task.Run(() =>
+            {
+                // some work
+                Thread.Sleep(2000);
+                return 42;
+            }, cts.Token);
+
+            // Cancel the task if needed
+            if ((new Random()).NextDouble() >= 0.5) cts.Cancel();
+
+            // Await the task and check properties
+            try
+            {
+                int result = await task;
+                Console.WriteLine($"Task completed successfully with result: {result}");
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine("Task failed with exceptions:");
+                foreach (var innerEx in ex.InnerExceptions)
+                {
+                    Console.WriteLine(innerEx.Message);
+                }
+            }
+            finally
+            {
+                // This Block will always execute even if no code from try and catch is executed
+                Console.WriteLine($"Task Status: {task.Status}");
+                Console.WriteLine($"Is Faulted: {task.IsFaulted}");
+                Console.WriteLine($"Is Canceled: {task.IsCanceled}");
+            }
+        }
+    }
+}
--- C_Sharp_Playground/Program.cs
+++ C_Sharp_Playground/Program.cs
@@ -1,16 +1,23 @@
-﻿using System;
+﻿using C_Sharp_Playground.TAP;
+using System;
+using System.Threading.Tasks;
 
 namespace C_Sharp_Playground
 {
-    class Program
+	class Program
 	{
-		static void Main(string[] args)
+		/*static void Main(string[] args)
 		{
             //new Indexers.IndexerProgram();
             //new GenericExtension.GenenricExtRunner();
             new LinqTryOuts.GroupOuterJoinWithSelectMany();
 
 			Console.ReadKey();
+		}*/
+
+		static async Task Main(string[] args)
+		{
+			await (new BasicTask()).DoTask();
 		}
 	}
 }
