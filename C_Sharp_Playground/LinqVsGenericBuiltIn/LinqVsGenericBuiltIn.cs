e749c310d28333926f5bf644a0fe3ebcdd2dfc23
WeaponXiii <samaun.xiii@gmail.com>
2024-12-01 03:57:15 +0100

    Add: Linq vs collations.Generic methods examples.

---
+++ C_Sharp_Playground/LinqVsGenericBuiltIn/LinqVsGenericBuiltIn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C_Sharp_Playground.LinqVsGenericBuiltIn
+{
+    class LinqVsGenericBuiltIn
+    {
+        public LinqVsGenericBuiltIn()
+        {
+            List<int> list = [1, 2, 3, 4, 5];
+
+            var list1 = list.Select(x => 2 * x).ToList(); // Better to use with iEnumerable
+            var list2 = list.ConvertAll(x => 2 * x); // Specific for already "materialized" list 
+
+            Console.WriteLine("list after Select().ToList()");
+            list1.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine($"{Environment.NewLine}list2.ForEach(x => Console.WriteLine(x)):");
+            list2.ForEach(x => Console.WriteLine(x));
+
+            /*
+             The List<T> class contains several methods that has almost exact matches in LINQ. 
+                Exists -> Any, Find -> First, FindAll -> Where, FindLast -> Last, TrueForAll -> All  
+             ConvertAll is available for arrays too, using the static "Array.ConvertAll(array, converter)"
+             */
+
+        }
+    }
+}
--- C_Sharp_Playground/Program.cs
+++ C_Sharp_Playground/Program.cs
@@ -2,11 +2,12 @@
 using System;
 using System.Threading.Tasks;
 
+
 namespace C_Sharp_Playground
 {
 	class Program
 	{
-		/*static void Main(string[] args)
+        /*static void Main(string[] args)
 		{
             //new Indexers.IndexerProgram();
             //new GenericExtension.GenenricExtRunner();
@@ -14,10 +15,18 @@ namespace C_Sharp_Playground
 
 			Console.ReadKey();
 		}*/
-
+        /*
 		static async Task Main(string[] args)
 		{
 			await (new BasicTask()).DoTask();
 		}
-	}
+		*/
+
+        // The following will not work for two reasons
+        //	i. The class is not public
+        // ii. LinqVsGenericBuiltIn will be recognized as namespace
+        //      even after a specific "using C_Sharp_Playground.LinqVsGenericBuiltIn;" statement
+        // static void Main() => new LinqVsGenericBuiltIn();
+        static void Main() => new LinqVsGenericBuiltIn.LinqVsGenericBuiltIn();
+    }
 }
